ba193931e4da37657a919c9f1ec03742822005a0
zozomika <33067047+zozomika@users.noreply.github.com>
2024-12-18 21:04:37 +0700

    Add files via upload

---
+++ sample.tcl
@@ -0,0 +1,27 @@
+source /home/FSH0AS064B_PR/hann_ne/FSH0AS064B/pr/work/create_outside_RB_avoid_signal_cross.tcl
+gcreate_RB_avoid_signal_pin_of_cell -cell  u040efulp00800833400 -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  u040efulp256016254o0 -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FZOTG266HH0L_LF -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FZOTG266HH0L_LF -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  CS_AD090320T_UMC40LP_A0 -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL120HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL120HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL120HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL360HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL362HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPLL510HH0L_DPHY -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDCDL201HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXADDLL200HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDCDL201HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXADDLL200HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXVDT0211HH0L_BP3 -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXVDT0211HH0L_BP3 -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXPORKLH530HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4DFD612HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4DFD612HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4DFD612HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4DFD612HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4COMP101HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXDDR4AFD612HH0L -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXADC1402HH0L_FC24A -offset 10
+gcreate_RB_avoid_signal_pin_of_cell -cell  FXTDCD10B04C00HH0L_CPB -offset 10
